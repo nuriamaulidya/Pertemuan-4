3937f059136ab3eae4f37cb0bb4ba868c027af21
nuriamaulidya <145628183+nuriamaulidya@users.noreply.github.com>
2023-10-27 09:32:58 +0700

    Add files via upload

---
+++ Pertemuan_4/src/Pertemuan4/Lingkaran.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pertemuan4;
+
+/**
+ *
+ * @author lenovo
+ */
+public class Lingkaran extends bangundatar2 {
+
+    float r;
+    }
---
+++ Pertemuan_4/src/Pertemuan4/PersegiPanjang.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Pertemuan4;
+
+/**
+ *
+ * @author lenovo
+ */
+public class PersegiPanjang extends bangundatar2{
+
+    float panjang;
+    float lebar;
+    
+}
